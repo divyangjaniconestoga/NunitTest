382fcebfb5d85c691ab7142d86396aed7c160b3e
asd <asd@DESKTOP-D512QCI>
2023-02-18 09:45:27 -0500

    Add project files.

---
+++ NunitTest/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleConfirmation
+{
+    public static class Triangle
+    {
+
+        static void Main(string[] args)
+        {
+        }
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+        {
+
+            string result;
+
+
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "This triangle is valid.";
+
+            }
+
+            else
+            {
+                result = "The triangle is NOT valid.";
+
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ TriangleTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleConfirmation;
+
+namespace TriangleTest
+{
+    [TestFixture]
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+        }
+
+        [Test]
+        public void ValidTriangle_Input60and60and60_ExpectedValidTriangle()
+        {
+            int firstangle = 60;
+            int secondangle = 60;
+            int thirdangle = 60;
+
+            string expected = "This triangle is valid.";
+            string actual=Triangle.ValidTriangle(firstangle, secondangle, thirdangle);
+            Assert.AreEqual(expected, actual);
+        
+        }
+    }
+}
